9d688f65f97d026759bf2fbd4c9703b4a4b471df
dinushchathurya <dinushchathurya21@gmail.com>
2020-11-08 14:41:02 +0530

    feat: configure express-session

--- server.js
+++ server.js
@@ -33,3 +33,5 @@ passport.deserializeUser((obj, cb) => {
 app.set('view engine', 'ejs');
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
+
+app.use(require('express-session')({ secret: SESSION_SECRET, resave: true, saveUninitialized: true }));
