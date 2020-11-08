0a8f7e8192ffb98e21fa158860c2e2baa741a91f
dinushchathurya <dinushchathurya21@gmail.com>
2020-11-08 14:33:53 +0530

    feat: import GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, SESSION_SECRET from .env to server.js

--- server.js
+++ server.js
@@ -5,3 +5,4 @@ const path = require('path');
 const express = require('express');
 const passport = require('passport');
 const { Strategy } = require('passport-google-oauth20');
+const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, SESSION_SECRET } = process.env;
