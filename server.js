019962d11fccff1ea177cacdacce1d615a1ac00c
dinushchathurya <dinushchathurya21@gmail.com>
2020-11-08 14:29:01 +0530

    feat: import installed dependencies to server.js

---
+++ server.js
@@ -0,0 +1,7 @@
+'use strict'
+
+require('dotenv').config()
+const path = require('path');
+const express = require('express');
+const passport = require('passport');
+const { Strategy } = require('passport-google-oauth20');
